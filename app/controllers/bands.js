1e1f9eebdf598ffa796f591637b2bcc6df2b27d8
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-10-09 21:23:31 -0700

    starting from scratch

--- app/controllers/bands.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  name: '',
-
-  isAddButtonDisabled: Ember.computed('name', function() {
-    return Ember.isEmpty(this.get('name'));
-  })
-});
--- app/router.js
+++ app/router.js
@@ -5,13 +5,6 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
-  this.route('bands', function() {
-    this.route('band', { path: ':slug' }, function() {
-      this.route('details');
-      this.route('songs');
-    });
-  });
-});
+Router.map(function() {});
 
 export default Router;
