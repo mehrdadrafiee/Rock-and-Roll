356e88f47b6464722721606ed28d2243a742382f
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2017-01-20 17:27:09 -0800

    Adds master-details view

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,14 +1,83 @@
 import Ember from 'ember';
 
+///////BAND STARTS///////
 var Band = Ember.Object.extend({
-  name: ''
+  name: '',
+
+  slug: Ember.computed('name', function() {
+    return this.get('name').dasherize();
+  })
 });
 
-var ledZeppelin = Band.create({ name: 'Led Zeppelin' });
-var pearlJam = Band.create({ name: 'Pearl Jam' });
-var fooFighters = Band.create({ name: 'Foo Fighters' });
+var BandsCollection = Ember.Object.extend({
+  content: [],
+  sortProperties: ['name:desc'],
+  sortedContent: Ember.computed.sort('content', 'sortProperties')
+});
 
-var bands = [ledZeppelin, pearlJam, fooFighters];
+var ledZeppelin = Band.create({
+  name: 'Led Zeppelin',
+  songs: [blackDog]
+});
+
+var pearlJam = Band.create({
+  name: 'Pearl Jam',
+  songs: [daughter, yellowLedbetter]
+});
+
+var fooFighters = Band.create({
+  name: 'Foo Fighters',
+  songs: [pretender]
+});
+
+var kayaProject = Band.create({
+  name: 'Kaya Project',
+  songs: [alwaysWaiting]
+});
+
+var bands = BandsCollection.create();
+bands.get('content').pushObjects([ ledZeppelin, pearlJam, fooFighters, kayaProject ]);
+///////BAND ENDS///////
+
+
+
+///////SONG STARTS///////
+var Song = Ember.Object.extend({
+  title: '',
+  band: '',
+  rating: 0
+});
+
+var blackDog = Song.create({
+  title: 'Black Dog',
+  band: 'Led Zeppelin',
+  rating: 3
+});
+
+var yellowLedbetter = Song.create({
+  title: 'Yellow Ledbetter',
+  band: 'Pearl Jam',
+  rating: 4
+});
+
+var daughter = Song.create({
+  title: 'Daughter',
+  band: 'Pearl Jam',
+  rating: 5
+});
+
+var pretender = Song.create({
+  title: 'The Pretender',
+  band: 'Foo Fighters',
+  rating: 2
+});
+
+var alwaysWaiting = Song.create({
+  title: 'Always Waiting',
+  band: 'Kaya Project',
+  rating: 5
+});
+///////SONG ENDS///////
 
 export default Ember.Route.extend({
   model() {
